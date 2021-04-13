ee79e35885a395d776fe413fe132b6e60ac99c58
DieBirgit <birgit.buedenhoelzer@posteo.de>
2021-04-13 20:59:15 +0200

    Vorderhaus01

---
+++ Vorderhaus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Floorset_B1" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="pics/Floorset_B1.jpg" width="192" height="64"/>
+</tileset>
